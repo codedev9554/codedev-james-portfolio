16f3d18651f14b0520a8d29ddd0ad05aeba11fc3
codedev9554 <code.dev620@gmail.com>
2023-10-25 06:20:51 -0700

    codedev-james-portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am James Lewis. </span>I am an experienced front-end UI developer specializing in React.js and Next.js development. With a strong background in building responsive and user-friendly web applications, I have successfully delivered numerous projects for clients across various industries."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
